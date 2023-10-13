adc325db34d4b935b94127b42bc509dd4085eafd
divyang <divyang0024@gmail.com>
2023-10-13 17:20:34 +0530

    complete pattern problem 1

---
+++ c++/patternproblem1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row = 5;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= row; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
