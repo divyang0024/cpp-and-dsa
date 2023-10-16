b9587123153125655ad44d7bc413c7fced43d646
divyang <divyang0024@gmail.com>
2023-10-16 12:51:47 +0530

    complete pattern problem 19,20 and 21

--- c++/patternproblem20.cpp
+++ c++/patternproblem20.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main()
 {
-    for (int row = 1; row <= 5; row += 1)
+    int n;
+    cout << "enter grid size : ";
+    cin >> n;
+    for (int row = 1; row <= n; row += 1)
     {
-        for (int col = 1; col <= 5; col += 1)
+        for (int col = 1; col <= n; col += 1)
         {
-            if (col <= 5 - row)
+            if (col <= n - row)
             {
                 cout << " ";
             }
             else
             {
-                cout << "*"
-                     << " ";
+                cout << row;
             }
         }
         cout << endl;
--- c++/practice/tempCodeRunnerFile.cpp
+++ c++/practice/tempCodeRunnerFile.cpp
@@ -1,4 +1,23 @@
- // else
-            // {
-            //     cout << " ";
-            // }
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row += 1)
+    {
+        for (int col = 1; col <= 5; col += 1)
+        {
+            if (col <= 5 - row + 1)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*"
+                     << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
