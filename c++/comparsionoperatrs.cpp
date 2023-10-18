7a795503ff2367f4d745b25d8d25c57be457fdc6
divyang <divyang0024@gmail.com>
2023-10-18 18:37:57 +0530

    complete unary,binary and comparison

---
+++ c++/comparsionoperatrs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 4;
+    if (a == b)
+    {
+        cout << "a is equal to b" << endl;
+    }
+    if (a > b)
+    {
+        cout << "a is greater than b" << endl;
+    }
+    if (a < b)
+    {
+        cout << "a is less than b" << endl;
+    }
+    if (a >= b)
+    {
+        cout << "a is greater than or equal to b" << endl;
+    }
+    if (a <= b)
+    {
+        cout << "a is less than or eqaul to b" << endl;
+    }
+
+    if (a != b)
+    {
+        cout << "a is not greater than b" << endl;
+    }
+    return 0;
+}
