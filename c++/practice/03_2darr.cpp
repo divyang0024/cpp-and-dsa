8a4442572e914607b79e41edfc92c89280270ccc
Divyang <divyang0024@gmail.com>
2024-03-22 17:26:46 +0530

    rotating 2d array clockwise and anticlockwise k times

--- c++/practice/03_2darr.cpp
+++ c++/practice/03_2darr.cpp
@@ -76,25 +76,31 @@ void printTransposedMatrix(int r, int c, int arr[][3])
     }
 }
 
-void rotateArrBy90Deg(int n, int arr[][3]) // leetcode problem 48
+void rotateArrBy90Deg(int n, vector<vector<int>> &arr) // leetcode problem 48
 {
-    int ans[3][3];
+    vector<vector<int>> temp = arr;
     for (int i = n - 1; i >= 0; i--)
     {
         for (int j = 0; j < n; j++)
         {
             // swap(arr[(n - 1) - i][j], arr[j][i]);
             // cout << ((n - 1) - i) << j << "->" << j << i;
-            ans[j][i] = arr[(n - 1) - i][j];
+            arr[j][i] = temp[(n - 1) - i][j];
         }
     }
-    for (int i = 0; i < n; i++)
+}
+void rotateArrBy90DegAnticlockwise(int n, vector<vector<int>> &arr) // leetcode problem 48
+{
+    vector<vector<int>> temp = arr;
+    for (int i = n - 1; i >= 0; i--)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << ans[i][j];
+            // cout << "[" << ((n - 1) - i) << j << "]"
+            //      << "->" << ((n - 1) - j) << ((n - 1) - i);
+            arr[(n - 1) - j][((n - 1) - i)] = temp[((n - 1) - i)][j];
+            // cout << endl;
         }
-        cout << endl;
     }
 }
 
@@ -119,6 +125,41 @@ void rotateArrBy180Deg(int n, int arr[][3])
     }
 }
 
+void rotateArrKtimesClockwiseAndAnticlockwise(int n, vector<vector<int>> &arr)
+{
+    char choice;
+    cout << "enter c for clockwise and a for anticlockwise : ";
+    cin >> choice;
+    int k;
+    cout
+        << "number of times you want to perform rotation : ";
+    cin >> k;
+    switch (choice)
+    {
+    case 'c':
+    {
+        for (int i = 0; i < k; i++)
+        {
+            rotateArrBy90Deg(n, arr);
+        }
+    }
+    break;
+    case 'a':
+    {
+        for (int i = 0; i < k; i++)
+        {
+            rotateArrBy90Deg(n, arr);
+        }
+    }
+    break;
+    default:
+    {
+        cout << "misinput!!!! please enter a valid expression.";
+        break;
+    }
+    }
+}
+
 int main()
 {
     int r, c;
@@ -126,7 +167,7 @@ int main()
     cin >> r;
     cout << "enter number of column : ";
     cin >> c;
-    int arr[r][3];
+    vector<vector<int>> arr(r, vector<int>(r, 0));
 
     for (int i = 0; i < r; i++)
     {
@@ -141,6 +182,16 @@ int main()
     // printSpiralForm(r, c, arr);
     // printTransposedMatrix(r, c, arr);
     // rotateArrBy90Deg(r, arr);
-    rotateArrBy180Deg(r, arr);
+    // rotateArrBy180Deg(r, arr);
+    // rotateArrBy90DegAnticlockwise(r, arr);
+    rotateArrKtimesClockwiseAndAnticlockwise(r, arr);
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < r; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
