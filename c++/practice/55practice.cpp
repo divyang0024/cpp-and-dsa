1692d85f6525876b119bf97b3cf3588a15ccc2da
Divyang <divyang0024@gmail.com>
2024-04-20 22:14:19 +0530

    find smallest distinct window in a string

--- c++/practice/55practice.cpp
+++ c++/practice/55practice.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-int largestCharContigousSubArray(string s)
+int largestCharContigousSubArray(string s) // leetcode problem 3.
 {
     int len = 0, first = 0, second = 0;
     vector<bool> count(256, 0);
@@ -21,8 +21,51 @@ int largestCharContigousSubArray(string s)
     return len;
 }
 
+int smallestDistinctWindow(string s)
+{
+    int len = s.size(), first = 0, second = 0, distinct = 0, i = 0;
+    vector<int> count(256, 0);
+    while (first < s.size()) // dinding number of distinct characters in a string.
+    {
+        if (count[s[first]] == 0)
+            distinct++;
+
+        count[s[first]]++;
+        first++;
+    }
+    first = 0;
+    for (int i = 0; i < 256; i++)
+        count[i] = 0;
+
+    while (second < s.size())
+    {
+        while (distinct && (second < s.size()))
+        {
+            if (count[s[second]] == 0)
+                distinct--;
+
+            count[s[second]]++;
+            second++;
+        }
+        len = min(len, second - first);
+        while (distinct != 1)
+        {
+            len = min(len, second - first);
+            count[s[first]]--;
+
+            if (count[s[first]] == 0)
+            {
+                distinct++;
+            }
+            first++;
+        }
+    }
+    return len;
+}
+
 int main()
 {
-    cout << largestCharContigousSubArray("dvdf");
+    // cout << largestCharContigousSubArray("dvdf");
+    cout << smallestDistinctWindow("aabbbcbbac");
     return 0;
 }
